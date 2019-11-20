a479906175f4dc69f1b6ac69ac30f38eb7f4232a
Dev Catalyst <44650216+DevCatalystTN@users.noreply.github.com>
2019-11-20 15:15:57 -0600

    Update 04-Populating-Your-Table.sql

--- 04-Populating-Your-Table.sql
+++ 04-Populating-Your-Table.sql
@@ -2,41 +2,143 @@ INSERT INTO products(productName, productInventory, productPrice)
     VALUES (‘Broccoli’, 17, 1.00), (‘Cabbage’, 15, .99);
     
 -- Content for employees, departmentsm and deptEmployees
-INSERT INTO employees(employeeFName, employeeLName, employeeTitle) VALUES ('John', 'Smith', 'Manager');
-INSERT INTO employees(employeeFName, employeeLName, employeeTitle) VALUES ('Spencer', 'Jones', 'Dairy Manager');
-INSERT INTO employees(employeeFName, employeeLName, employeeTitle) VALUES ('Karen', 'Fitzpatrick', 'Courtesy Clerk');
-INSERT INTO employees(employeeFName, employeeLName, employeeTitle) VALUES ('Meghan', 'Lake', 'Courtesy Clerk');
-INSERT INTO employees(employeeFName, employeeLName, employeeTitle) VALUES ('David', 'Hasslehoff', 'Produce Manager');
-INSERT INTO employees(employeeFName, employeeLName, employeeTitle) VALUES ('Bartholomew', 'Moore', 'Frozen Manager');
-INSERT INTO employees(employeeFName, employeeLName, employeeTitle) VALUES ('Kelly', 'Moore', 'Customer Service Clerk');
-INSERT INTO employees(employeeFName, employeeLName, employeeTitle) VALUES ('Peter', 'Parker', 'Deli Manager');
-INSERT INTO employees(employeeFName, employeeLName, employeeTitle) VALUES ('Felicia', 'Hardy', 'Baker');
-INSERT INTO employees(employeeFName, employeeLName, employeeTitle) VALUES ('Silas', 'Bridger', 'Head Pharmacist');
-INSERT INTO employees(employeeFName, employeeLName, employeeTitle) VALUES ('Naomi', 'Fujiwara', 'Meat Clerk');
-INSERT INTO employees(employeeFName, employeeLName, employeeTitle) VALUES ('Tasgal', 'McRoth', 'Butcher');
+INSERT INTO aisles(aisleName, refridgerated) VALUES (1, 1);--Produce
+INSERT INTO aisles(aisleName, refridgerated) VALUES (2, 0);--International/Pasta
+INSERT INTO aisles(aisleName, refridgerated) VALUES (3, 0);--Bread/Cereal
+INSERT INTO aisles(aisleName, refridgerated) VALUES (4, 0);--Coffee/juice boxes/fruit snacks
+INSERT INTO aisles(aisleName, refridgerated) VALUES (5, 0);--Baking
+INSERT INTO aisles(aisleName, refridgerated) VALUES (6, 0);--Vegetables/Fruit/Tuna
+INSERT INTO aisles(aisleName, refridgerated) VALUES (7, 0);--Chips/Popcorn/Snacks
+INSERT INTO aisles(aisleName, refridgerated) VALUES (8, 0);--Candy/Cookies
+INSERT INTO aisles(aisleName, refridgerated) VALUES (9, 0);--Liquor
+INSERT INTO aisles(aisleName, refridgerated) VALUES (10, 0);--Cleaning Supplies
+INSERT INTO aisles(aisleName, refridgerated) VALUES (11, 0);--Shampoo/Deodorant/toothpaste
+INSERT INTO aisles(aisleName, refridgerated) VALUES (12, 0);--Holiday/magazines
+INSERT INTO aisles(aisleName, refridgerated) VALUES (13, 0);--pet care
+INSERT INTO aisles(aisleName, refridgerated) VALUES (14, 0);--Soft Drinks
+INSERT INTO aisles(aisleName, refridgerated) VALUES (15, 1);--Dairy
+INSERT INTO aisles(aisleName, refridgerated) VALUES (16, 1);--Deli/Meat
+INSERT INTO aisles(aisleName, refridgerated) VALUES (17, 1);--Frozen Vegetables/Frozen Meats
+INSERT INTO aisles(aisleName, refridgerated) VALUES (18, 1);--Frozen Dinners/Breakfasts/Lunches
+INSERT INTO aisles(aisleName, refridgerated) VALUES (19, 1);--Frozen Deserts
 
 
-INSERT INTO departments(deptName) VALUES ('Produce');
-INSERT INTO departments(deptName) VALUES ('Meat');
-INSERT INTO departments(deptName) VALUES ('Dairy');
-INSERT INTO departments(deptName) VALUES ('Deli');
-INSERT INTO departments(deptName) VALUES ('Frozen');
-INSERT INTO departments(deptName) VALUES ('Bakery');
-INSERT INTO departments(deptName) VALUES ('Front End');
-INSERT INTO departments(deptName) VALUES ('Grocery');
-INSERT INTO departments(deptName) VALUES ('Customer Service');
-INSERT INTO departments(deptName) VALUES ('Pharmacy');
-
-
-INSERT INTO deptEmployees(employeeID, deptID) VALUES (1, 9);
-INSERT INTO deptEmployees(employeeID, deptID) VALUES (2, 3);
-INSERT INTO deptEmployees(employeeID, deptID) VALUES (3, 7);
-INSERT INTO deptEmployees(employeeID, deptID) VALUES (4, 7);
-INSERT INTO deptEmployees(employeeID, deptID) VALUES (5, 1);
-INSERT INTO deptEmployees(employeeID, deptID) VALUES (6, 5);
-INSERT INTO deptEmployees(employeeID, deptID) VALUES (7, 9);
-INSERT INTO deptEmployees(employeeID, deptID) VALUES (8, 4);
-INSERT INTO deptEmployees(employeeID, deptID) VALUES (9, 6);
-INSERT INTO deptEmployees(employeeID, deptID) VALUES (10, 10);
-INSERT INTO deptEmployees(employeeID, deptID) VALUES (11, 2);
-INSERT INTO deptEmployees(employeeID, deptID) VALUES (12, 2);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Apple", 217, 1.25, 1);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Guava", 71, 2.25, 1);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Pasta Sauce", 197, 1.89, 2);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Kumquat", 83, 0.59, 1);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Taco Shells", 9000, 1.59, 2);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Pumpkin", 73, 3.99, 1);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Camel Milk", 74, 5.99, 15);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Chocolate Milk half", 321, 1.79, 15);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Whole Milk half", 189, 1.79, 15);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("2% Milk half", 245, 1.79, 15);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Skim Milk half", 289, 1.25, 15);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Kale", 97, 1.29, 1);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Ground Cinnamon", 87, 3.25, 5);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Green Onion", 167, 0.89, 1);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Green Bell Pepper", 426, 1.89, 1);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Red Bell Pepper", 317, 2.19, 1);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Yellow Bell Pepper", 193, 2.19, 1);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Cheddar Cheese Shred", 148, 3.99, 15);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Mozzarella Cheese Shred", 237, 3.99, 15);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Halloween Candy", 1987, 9.25, 12);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Prime Steak", 90, 12.59, 16);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Solid Gold-blum Statue", 8675309, 499.99, 12);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Tortilla Chips", 193, 1.99, 7);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Kettle Chips", 420, 2.69, 7);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Doritos", 308, 1.88, 7);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Cheetos", 111, 1.88, 7);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Pretzels", 76, 2.49, 7);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Lays", 207, 2.99, 7);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Spooky Decor", 1121, 59.99, 12);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Dr. Salt", 186, 1.33, 14);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Brad's Drink", 237, 1.33, 14);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Coca", 314, 1.33, 14);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Grape Squelch", 321, 1.33, 14);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Hill Moisture", 681, 1.33, 14);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Fairy", 139, 1.33, 14);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Himalaya Steam", 0, 1.33, 14);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Apple Squelch", 17, 1.33, 14);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Rice Noodles", 427, 1.19, 2);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Soy Sauce", 77, 1.59, 2);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Pork Butt", 37, 10.99, 16);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Chicken Wings", 49, 8.99, 16);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Bacon", 777, 6.79, 16);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Pork Ribs", 87, 13.99, 16);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Canadian Bacon", 216, 2.50, 16);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Stew Beef", 27, 8.99, 16);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Chicken Breast", 57, 4.59, 16);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Gushers", 129, 2.49, 4);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Fruity Frowns", 176, 1.79, 4);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Kool Aid", 276, 1.00, 4);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Little Hugs", 70, 5.99, 4);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("HiC", 21, 3.99, 4);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Capri Sun", 117, 2.99, 4);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Wonder Bread", 199, 1.69, 3);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Bagguette", 135, 1.89, 3);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Lucky Charms", 313, 2.89, 3);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Cheerios", 212, 2.89, 3);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Frosted Flakes", 132, 2.89, 3);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Apple Jacks", 96, 2.79, 3);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Hamburger Buns", 54, 1.89, 3);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Hotdog Buns", 78, 1.89, 3);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Baking Powder", 97, 2.49, 5);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Sugar", 333, 2.99, 5);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Flour", 238, 4.99, 5);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Paprika", 100, 1.89, 5);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Vinegar", 117, 6.99, 5);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Chicken Stock", 317, 2.69, 5);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Oil", 127, 2.89, 5);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Coffee", 1217, 8.99, 4);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Vanilla Ice Cream", 150, 2.99, 19);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Chocolate Ice Cream", 117, 2.99, 19);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Bacon Ice Cream", 135, 2.99, 19);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("PB Ice Cream", 142, 2.99, 19);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Klondike Bars", 109, 2.99, 19);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Italian Ice", 132, 1.99, 19);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Banana Cream Pie", 19, 3.79, 19);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Pie Crust", 67, 2.79, 19);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Lasagna", 13, 9.99, 18);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Taquitos", 211, 9.99, 18);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Pepporoni Pizza", 312, 3.99, 18);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Cheese Pizza", 132, 3.99, 18);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("BBQ N Gouda Pizza", 175, 3.99, 18);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Hot Pouches", 327, 5.99, 18);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Eggo", 387, 1.99, 18);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Egg McMuffin", 252, 1.89, 18);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Eggs", 400, 1.59, 15);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Butter", 243, 3.99, 15);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Skittles", 197, 2.25, 8);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Candy Corn", 117, 0.99, 8);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Orange Slices", 185, 0.99, 8);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Snickers", 284, 3.89, 8);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("M&Ms", 207, 2.50, 8);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Chocolate Chip Cook", 67, 2.50, 8);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Snickerdoodle Cook", 83, 2.50, 8);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Tuna", 380, .59, 6);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Apples Sliced", 57, 1.50, 6);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Pears Sliced", 68, 1.50, 6);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Cherries", 90, 2.99, 6);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Fruit Salad", 0, 3.99, 6);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Peas", 27, 0.99, 6);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Green Beans", 217, 1.25, 6);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Artichokes", 12, 1.99, 6);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Frozen Carrots", 114, 1.69, 17);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Stew Vegetables", 271, 2.79, 17);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Frozen Brocolli", 231, 1.69, 17);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Frozen Corn", 183, 1.69, 17);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Chicken Thighs", 69, 6.99, 17);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Sausage", 52, 2.99, 17);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Chorizo", 77, 2.49, 17);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Maple Ham", 37, 7.99, 16);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Honey Turkey", 23, 5.99, 16);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Bleach", 207, 1.99, 10);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Tide Pods", 69, 13.99, 10);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Paper Towels", 120, 8.99, 10);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Trash Bags", 217, 14.99, 10);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Lysol", 271, 2.79, 10);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Paper Plates", 177, 9.99, 10);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Mr. Bubble", 139, 4.25, 11);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Toothpaste", 217, 3.29, 11);
+INSERT INTO productTest(productName, productInventory, productPrice, aisleID) VALUES ("Apple", 217, 1.25, 1);
